4d1db670bbf69779e487c621fbccbd4fac5e53ce
Daniel Grbac Bravo <danielgrbacbravo@gmail.com>
2023-10-13 23:45:05 +0200

    feat: added comments and nvimtree

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -16,17 +16,13 @@ use 'hrsh7th/vim-vsnip'
 use {'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true }}
 use 'mfussenegger/nvim-dap'
 use 'sitiom/nvim-numbertoggle'
-use {
-        'nvim-treesitter/nvim-treesitter',
-        run = function()
-            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-            ts_update()
-        end,
-    }
+use {'nvim-treesitter/nvim-treesitter', 
+run = function() 
+local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+ts_update()
+end,}
 use 'mfussenegger/nvim-lint'
-use {
-  "ray-x/lsp_signature.nvim",
-}
+use "ray-x/lsp_signature.nvim"
 use {
     'goolord/alpha-nvim',
     requires = { 'nvim-tree/nvim-web-devicons' },
@@ -34,6 +30,28 @@ use {
         require'alpha'.setup(require'alpha.themes.startify'.config)
     end
 }
-use { "catppuccin/nvim", as = "catppuccin" }
-use {'nyoom-engineering/oxocarbon.nvim'}
+use { 
+  'olivercederborg/poimandres.nvim',
+  config = function()
+    require('poimandres').setup {
+      -- leave this setup function empty for default config
+      -- or refer to the configuration section
+      -- for configuration options
+    }
+  end
+}
+use "lukas-reineke/indent-blankline.nvim"
+-- Nvim tree so i can see my files
+use {
+  'nvim-tree/nvim-tree.lua',
+  requires = {
+    'nvim-tree/nvim-web-devicons', -- optional
+  },
+}
+
+use {"folke/todo-comments.nvim"}
+use {
+	"windwp/nvim-autopairs",
+    config = function() require("nvim-autopairs").setup {} end
+}
 end)
